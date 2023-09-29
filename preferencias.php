a76690a8bd077db04131dd7dd299fd670aabf988
fernando <fernandoat90@gmail.com>
2023-09-28 21:46:40 -0300

    Agregando cambios a la rama torres

--- preferencias.php
+++ preferencias.php
@@ -3,9 +3,11 @@ require ("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$pc=(isset($_GET['pc'])?$_GET['pc']:"No cargaste un nombre de pc");
 
-echo "Hola $usuario";
-echo "Tu color preferido es: $color";
+echo "Hola $usuario ";
+echo " Tu color preferido es: $color ";
+echo " Y tu pc es marca: $pc";
 
 
 
