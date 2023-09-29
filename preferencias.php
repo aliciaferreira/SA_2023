5a536584ec1fd8567646684c9b21c170ef7a71d2
Galo98 <galonahuel98@gmail.com>
2023-09-28 21:55:33 -0300

    cambios solicitados

--- preferencias.php
+++ preferencias.php
@@ -3,9 +3,11 @@ require ("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$agregado = (isset($_GET['agre']) ? $_GET['agre'] : "No cargaste el campo agregado");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
+echo "Campo agregado es : $agregado";
 
 
 
