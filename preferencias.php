d2b01d32978bbbef8ba7dbb16d27ffad491fffce
Celeste Diaz <celestenoelia@gmail.com>
2023-10-12 20:39:24 -0300

    cambios

--- preferencias.php
+++ preferencias.php
@@ -3,10 +3,10 @@ require ("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
-
+$edad=(isset($_GET['edad'])?$_GET['edad']:"No cargaste una edad preferido");
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
-
+echo "Tu edad preferida es: $edad";
 
 
 
