7d600d5b182ab5ae0cccb38b2aac5d59e15c197e
Dana More <danamore@live.com>
2023-10-12 20:50:11 -0300

    cambios

--- preferencias.php
+++ preferencias.php
@@ -1,14 +1,10 @@
 <?php
-require ("preferencias.html");
+require("preferencias.html");
 
-$usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
-$color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$usuario = (isset($_GET['usuario']) ? $_GET['usuario'] : "No cargaste un nombre de usuario");
+$color = (isset($_GET['color']) ? $_GET['color'] : "No cargaste un color preferido");
+$pelicula = (isset($_GET['pelicula']) ? $_GET['pelicula'] : "No cargaste tus películas de preferencia");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
-
-
-
-
-
-?>
+echo "Tu película de preferencia es: " . $pelicula;
