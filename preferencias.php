a263013af3115dde1de260a8508e46d0df352f0e
Catriel <catrielavalos@gmail.com>
2023-10-16 02:42:31 -0300

    Se inicia el proyecto

--- preferencias.php
+++ preferencias.php
@@ -4,11 +4,14 @@ require ("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
+$deporte=(isset($_GET['deporte'])?$_GET['deporte']:"No cargaste un deporte preferido o jovie");
+
 
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
 echo "Tu banda preferida es: $banda";
+echo "Tu deporte preferida es: $deporte";
 
 
 
