39b0138881a705354f14c41bba2c34b5552e1369
aliciaferreira <alicia.ferreira.sistemas@gmail.com>
2023-10-19 21:06:40 -0300

    add pc in preferencias and clean code

--- preferencias.php
+++ preferencias.php
@@ -4,19 +4,11 @@ require ("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $pc=(isset($_GET['pc'])?$_GET['pc']:"No cargaste un nombre de pc");
+$banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
 
 echo "Hola $usuario ";
 echo " Tu color preferido es: $color ";
+echo "Tu banda preferida es: $banda ";
 echo " Y tu pc es marca: $pc";
-$banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
-
-
-echo "Hola $usuario";
-echo "Tu color preferido es: $color";
-echo "Tu banda preferida es: $banda";
-
-
-
-
 
 ?>
