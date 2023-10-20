bc5e23bbc3c8e671c4f1b73b17dcb2c6ed7759c1
fernando <fernandoat90@gmail.com>
2023-10-19 21:07:11 -0300

    Agregando cambios a mi rama

--- preferencias.php
+++ preferencias.php
@@ -4,17 +4,13 @@ require ("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $pc=(isset($_GET['pc'])?$_GET['pc']:"No cargaste un nombre de pc");
-
-echo "Hola $usuario ";
-echo " Tu color preferido es: $color ";
-echo " Y tu pc es marca: $pc";
 $banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
 
 
-echo "Hola $usuario";
-echo "Tu color preferido es: $color";
-echo "Tu banda preferida es: $banda";
-
+echo "<br>Hola $usuario <br>";
+echo "Tu color preferido es: $color <br>";
+echo "Tu banda preferida es: $banda <br>";
+echo " Y tu pc es marca: $pc <br>";
 
 
 
