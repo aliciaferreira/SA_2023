11dd94e3d43686cd7f92504361cd3422863037e6
luciano <luciano.herdeli5@gmail.com>
2023-10-26 21:10:19 -0300

    agrego cambios

--- preferencias.php
+++ preferencias.php
@@ -3,10 +3,11 @@ require ("preferencias.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
+$club=(isset($_GET['club'])?$_GET['club']:"No cargaste un club");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
-
+echo "Sos hincha de: $club";
 
 
 
