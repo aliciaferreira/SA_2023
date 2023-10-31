fcc35e36908b135264fc89c10da179568a65376b
Gonzalo <99339459+madeforyalo@users.noreply.github.com>
2023-10-31 11:19:50 -0300

    tipo de Gonza preferido

--- preferencias.php
+++ preferencias.php
@@ -4,11 +4,13 @@ require ("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
+$gonza=(isset($_GET['gonza'])?$_GET['gonza']:"No cargaste un tipo de Gonza preferido");
 
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
 echo "Tu banda preferida es: $banda";
+echo "Tu Gonza preferido es: $gonza";
 
 
 
