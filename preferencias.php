15a61098d1bb808d238a261340cfcac91e3df1c2
Mariano Facundo Morbelli <144305761+Morbelli@users.noreply.github.com>
2023-11-06 22:11:10 -0300

    Partido

--- preferencias.php
+++ preferencias.php
@@ -4,12 +4,12 @@ require ("preferencias.html");
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
 $banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
-
+$partido=(isset($_GET['banda'])?$_GET['banda']:"No cargaste un partido político");
 
 echo "Hola $usuario";
 echo "Tu color preferido es: $color";
 echo "Tu banda preferida es: $banda";
-
+echo "Tu Partido Político es: $partido";
 
 
 
